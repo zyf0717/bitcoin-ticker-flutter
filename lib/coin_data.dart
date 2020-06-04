fb7b6497f3608d729c656cbb36d324162d6b62e4
zyf0717 <zyf0717@gmail.com>
2020-06-04 12:56:11 +0800

    Initial upload.

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,4 +1,8 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 const List<String> currenciesList = [
+  'Select currency',
   'AUD',
   'BRL',
   'CAD',
@@ -28,4 +32,25 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const apiKey = '86BC0EBB-A500-4440-AAEE-B5F3E686EC77';
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+
+class CoinData {
+  Future getRates(String currency) async {
+    Map<String, String> cryptoPrices = {};
+    for (var crypto in cryptoList) {
+      String url =
+          '$coinAPIURL/$crypto/$currency?apikey=63CF6F4D-3B7E-4FC7-BD07-13160B622FAD';
+      http.Response response = await http.get(url);
+      if (response.statusCode == 200) {
+        print('Checking $crypto to $currency rates...');
+        var decodedData = jsonDecode(response.body);
+        double price = decodedData['rate'];
+        cryptoPrices[crypto] = price.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+      }
+    }
+    return cryptoPrices;
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'coin_data.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,45 +10,140 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+  var exchangeRates;
+  var displayedCards;
+  bool isWaiting = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  DropdownButton<String> androidDropdown() {
+    List<DropdownMenuItem<String>> dropdownMenuItemList = [
+      for (String currency in currenciesList)
+        DropdownMenuItem(
+          child: Text(currency),
+          value: currency,
+        )
+    ];
+    return DropdownButton<String>(
+        value: selectedCurrency,
+        items: dropdownMenuItemList,
+        onChanged: (value) async {
+          setState(() {
+            selectedCurrency = value;
+            getData();
+          });
+        });
+  }
+
+  CupertinoPicker iOSPicker() {
+    List<Widget> pickerList = [
+      for (String currency in currenciesList) Text(currency)
+    ];
+    return CupertinoPicker(
+      itemExtent: 35.0,
+      onSelectedItemChanged: (selected) async {
+        setState(() {
+          selectedCurrency = currenciesList[selected];
+          getData();
+        });
+      },
+      children: pickerList,
+    );
+  }
+
+  void getData() async {
+    isWaiting = true;
+    setState(() {});
+    try {
+      var data = await CoinData().getRates(selectedCurrency);
+      isWaiting = false;
+      setState(() {
+        exchangeRates = data;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  List<StatelessWidget> getDisplayedCards() {
+    List<StatelessWidget> cardsList = [];
+    for (var crypto in cryptoList) {
+      cardsList.add(ConversionCards(
+        exchangeRate: isWaiting ? '?' : exchangeRates[crypto],
+        selectedCurrency: selectedCurrency,
+        crypto: crypto,
+      ));
+    }
+    return cardsList;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('🤑 Coin Ticker'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
+      body: ModalProgressHUD(
+        color: Colors.black87,
+        inAsyncCall: isWaiting,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: getDisplayedCards(),
               ),
             ),
+            Container(
+              height: 150.0,
+              alignment: Alignment.center,
+              color: Colors.lightBlue,
+              child: Platform.isIOS ? iOSPicker() : androidDropdown(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ConversionCards extends StatelessWidget {
+  const ConversionCards(
+      {this.exchangeRate, this.selectedCurrency, this.crypto});
+
+  final String exchangeRate;
+  final String selectedCurrency;
+  final String crypto;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+      child: Card(
+        color: Colors.lightBlueAccent,
+        elevation: 5.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          child: Text(
+            '1 $crypto = $exchangeRate $selectedCurrency',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
           ),
-          Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
-          ),
-        ],
+        ),
       ),
     );
   }
